97eefd632d7d9f0a546f99bcd8eefacf757eb07b
yunqian44 <yunqian8@live.com>
2020-07-13 17:11:32 +0800

    添加项目文件。

---
+++ CnBateBlogWeb/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CnBateBlogWeb.Models;
+using Microsoft.AspNetCore.Hosting;
+
+namespace CnBateBlogWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWebHostEnvironment _env;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment env)
+        {
+            _env = env;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var s= $"当前系统处于什么环境，开发:{_env.IsDevelopment()}-----生成环境:{_env.IsProduction()}";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
